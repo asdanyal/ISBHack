f73b31a835838088c4669ff5dee908a3ccb21c58
A.S.Danyal <12bscsadanyal@seecs.edu.pk>
2016-07-16 13:05:23 +0500

    first commit

---
+++ app/src/test/java/isbhack/a2016/isbhack2016/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package isbhack.a2016.isbhack2016;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
